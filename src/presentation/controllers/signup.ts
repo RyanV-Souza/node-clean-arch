4a8f2dc2ae49c408f14ee740e5532a3dc6637346
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2024-08-26 13:14:53 -0300

    feat: ensure signupcontroller returns 400 if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,22 @@
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle(httpRequest: any): any {
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing params: name'),
+      }
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing params: email'),
+      }
+    }
+
     return {
       statusCode: 400,
-      body: new Error('Missing params: name')
+      body: new Error('Missing params: name'),
     }
   }
 }
