8273b67047d1ebca1f3455120fd6e4972f40cff6
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2024-08-26 21:58:43 -0300

    chore: add jest config and scripts

--- jest.config.ts
+++ jest.config.ts
@@ -7,14 +7,19 @@ import type { Config } from 'jest'
 
 const config: Config = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+  ],
   collectCoverage: false,
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   coverageProvider: 'v8',
   transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
+    '.+\\.ts$': 'ts-jest',
+  },
 }
 
 export default config
