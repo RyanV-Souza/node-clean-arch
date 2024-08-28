c2cb71d5baa2c2ae12152dc5ba84964873d57af2
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2024-08-27 21:52:29 -0300

    feat: ensure content-type default as json

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Content-Type Middleware', () => {
+  it('Should return default content-type as json', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send('')
+    })
+
+    const response = await request(app).get('/test_content_type')
+
+    expect(response.headers['content-type']).toEqual(
+      'application/json; charset=utf-8',
+    )
+  })
+})
